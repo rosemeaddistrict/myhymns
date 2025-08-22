354c14ea7f320a8e5e3215f74fa42b7662ec028e
bobkkblue <chienyec@gmail.com>
2025-08-21 21:53:53 -0700

    update

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ async function loadCsvData(filename) {
         document.getElementById(lyris).innerText = 'Error loading CSV data.';
     }
 }
+
+function playAudio(t1, t2){
+    console.log("Play audio...");
+    document.getElementById("player").src = `${audio}#t=${t1},${t2}`;
+    document.getElementById("player").play();
+}
+
+function playYoutube(youtube, videoW, videoH, t1, t2){
+    console.log("Play youtube...");
+    const videoSetting = `${youtube};start=${t1}&end=${t2}`;
+    document.getElementById("youtube").innerHTML = `<iframe width=\"${videoW}\" height=\"${videoH}\" src=\"${videoSetting}\"" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+}
+
+function showScore() {
+    const score = document.getElementById("score").checked;
+    const obj = document.getElementById("pdf");
+    score ? obj.style.visibility = "visible" : obj.style.visibility = "hidden";
+}
